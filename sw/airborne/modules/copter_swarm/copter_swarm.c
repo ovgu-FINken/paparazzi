e37a6eb49f408267d6875cdf5b3aed8ec191281f
finkeneers <finkeneers@iws.cs.ovgu.de>
2017-08-16 16:12:04 +0200

    started with copter_swarm module

--- sw/airborne/modules/copter_swarm/copter_swarm.c
+++ sw/airborne/modules/copter_swarm/copter_swarm.c
@@ -6,37 +6,60 @@
 #include <dl_protocol.h>
 
 
-void copter_swarm_init( void ) {
+//ins_node_t* list;
 
+void copter_swarm_init( void ) {
+    //list = NULL;
 }
+
 
 void copter_ins_action(void) {
 	// we already know own position so skip this.
 	if(DL_COPTER_INS_ac_id(dl_buffer) == AC_ID)
 		return;
-
-	// gather all recieved INS values
+/*
+	// gather all recieved INS values and store them in the list
 	int ac_id = DL_COPTER_INS_ac_id(dl_buffer);
+	ins_node_t* copter_ins = find_ins_node(ac_id, list);
 
-	int ins_x = DL_COPTER_INS_ins_x(dl_buffer);
-	int ins_y = DL_COPTER_INS_ins_y(dl_buffer);
-	int ins_z = DL_COPTER_INS_ins_z(dl_buffer);
+	copter_ins->ins_x = DL_COPTER_INS_ins_x(dl_buffer);
+	copter_ins->ins_y = DL_COPTER_INS_ins_y(dl_buffer);
+	copter_ins->ins_z = DL_COPTER_INS_ins_z(dl_buffer);
 
-	int ins_xd = DL_COPTER_INS_ins_xd(dl_buffer);
-	int ins_yd = DL_COPTER_INS_ins_yd(dl_buffer);
-	int ins_zd = DL_COPTER_INS_ins_zd(dl_buffer);
+	copter_ins->ins_xd = DL_COPTER_INS_ins_xd(dl_buffer);
+	copter_ins->ins_yd = DL_COPTER_INS_ins_yd(dl_buffer);
+	copter_ins->ins_zd = DL_COPTER_INS_ins_zd(dl_buffer);
 
-	int ins_xdd = DL_COPTER_INS_ins_xdd(dl_buffer);
-	int ins_ydd = DL_COPTER_INS_ins_ydd(dl_buffer);
-	int ins_zdd = DL_COPTER_INS_ins_zdd(dl_buffer);
-
-	// and store them
-	
-	
-}
+	copter_ins->ins_xdd = DL_COPTER_INS_ins_xdd(dl_buffer);
+	copter_ins->ins_ydd = DL_COPTER_INS_ins_ydd(dl_buffer);
+	copter_ins->ins_zdd = DL_COPTER_INS_ins_zdd(dl_buffer);
+*/}
 
 void copter_swarm_periodic(void)
 {
 	
-
 }
+/*
+ins_node_t* find_ins_node(int ac_id, ins_node_t* first)
+{
+	if(first == NULL){
+		// TODO ask christoph where to free this memory if needed
+		first = malloc(sizeof(ins_node_t));
+		first->ac_id = ac_id;
+	}
+
+	ins_node_t* node = first;
+	while (node->next != NULL){
+		if(ac_id == node->ac_id)
+			return node;
+		node = node->next;
+	}
+	ins_node_t* new_node = malloc(sizeof(ins_node_t));
+	new_node->ac_id = ac_id;
+	node->next = new_node;
+	return new_node;	
+}
+*/
+
+//void copter_swarm_deinit( void ) {}
+
--- sw/airborne/modules/copter_swarm/copter_swarm.h
+++ sw/airborne/modules/copter_swarm/copter_swarm.h
@@ -5,5 +5,25 @@
 void copter_swarm_init(void);
 void copter_ins_action(void);
 void copter_swarm_periodic(void);
+/*
+typedef struct ins_node {
 
+    int ac_id;
+    int ins_x;
+    int ins_y;
+    int ins_z;
+    int ins_xd;
+    int ins_yd;
+    int ins_zd;
+    int ins_xdd;
+    int ins_ydd;
+    int ins_zdd;
+
+    struct ins_node* next;
+}ins_node_t;
+
+// returns the node with the given ac_id or a newly created one
+ins_node_t* find_ins_node(int ac_id, ins_node_t* list);
+
+*/
 #endif
