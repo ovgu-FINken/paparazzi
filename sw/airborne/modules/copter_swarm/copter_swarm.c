39cec272ef8aecd70696042d090bddb7a68df753
finkeneers <finkeneers@iws.cs.ovgu.de>
2017-08-21 11:04:00 +0200

    used by reference for struct in fun

--- sw/airborne/modules/copter_swarm/copter_swarm.c
+++ sw/airborne/modules/copter_swarm/copter_swarm.c
@@ -33,8 +33,10 @@ void copter_ins_action(void) {
 
 	// gather all recieved INS values and store them in the list
 	int ac_id = DL_COPTER_INS_ac_id(dl_buffer);
+	//To use call by reference manage memory
 	ins_node_t* copter_ins = find_ins_node(ac_id, list);
 /*
+	
 	copter_ins->ins_x = DL_COPTER_INS_ins_x(dl_buffer);
 	copter_ins->ins_y = DL_COPTER_INS_ins_y(dl_buffer);
 	copter_ins->ins_z = DL_COPTER_INS_ins_z(dl_buffer);
@@ -78,24 +80,68 @@ void calcForce(ins_node_t* copter0, ins_node_t* copter1){
 	// To return fx and fy
 }
 
-ins_node_t* find_ins_node(int ac_id, ins_node_t* first)
+ins_node_t* find_ins_node(int ac_id, ins_node_t* copter_ins)
 {
-	if(first == NULL){
+
+	//ins_node_t* ins = &copter_ins;
+	/*
+	//Value of the address 
+	while (((ins)->ac_id) != ac_id && &(ins)->next != NULL)
+		ins =&(ins)->next;		
+
+	//Check if the copter_ins value is NULL
+
+	
+	copter_ins->ins_x = DL_COPTER_INS_ins_x(dl_buffer);
+	copter_ins->ins_y = DL_COPTER_INS_ins_y(dl_buffer);
+	copter_ins->ins_z = DL_COPTER_INS_ins_z(dl_buffer);
+
+	copter_ins->ins_xd = DL_COPTER_INS_ins_xd(dl_buffer);
+	copter_ins->ins_yd = DL_COPTER_INS_ins_yd(dl_buffer);
+	copter_ins->ins_zd = DL_COPTER_INS_ins_zd(dl_buffer);
+
+	copter_ins->ins_xdd = DL_COPTER_INS_ins_xdd(dl_buffer);
+	copter_ins->ins_ydd = DL_COPTER_INS_ins_ydd(dl_buffer);
+	copter_ins->ins_zdd = DL_COPTER_INS_ins_zdd(dl_buffer);
+
+			
+	return copter_ins;
+*/
+	
+	
+	if(copter_ins == NULL){
 		// TODO ask christoph where to free this memory if needed
 		//first = malloc(sizeof(ins_node_t));
-		first->ac_id = ac_id;
+		copter_ins->ac_id = ac_id;
 	}
 
-	ins_node_t* node = first;
-	while (node->next != NULL){
-		if(ac_id == node->ac_id)
-			return node;
-		node = node->next;
-	}
+	ins_node_t* node = copter_ins;
+	while (copter_ins->next != NULL){
+		if(ac_id == copter_ins->ac_id)
+		{
+
+			copter_ins->ins_x = DL_COPTER_INS_ins_x(dl_buffer);
+			copter_ins->ins_y = DL_COPTER_INS_ins_y(dl_buffer);
+			copter_ins->ins_z = DL_COPTER_INS_ins_z(dl_buffer);
+
+			copter_ins->ins_xd = DL_COPTER_INS_ins_xd(dl_buffer);
+			copter_ins->ins_yd = DL_COPTER_INS_ins_yd(dl_buffer);
+			copter_ins->ins_zd = DL_COPTER_INS_ins_zd(dl_buffer);
+
+			copter_ins->ins_xdd = DL_COPTER_INS_ins_xdd(dl_buffer);
+			copter_ins->ins_ydd = DL_COPTER_INS_ins_ydd(dl_buffer);
+			copter_ins->ins_zdd = DL_COPTER_INS_ins_zdd(dl_buffer);
+			
+			return copter_ins;
+		}
+		copter_ins = copter_ins->next;
+		}
+
 	ins_node_t *new_node; //= (ins_node_t *) malloc(sizeof(ins_node_t));
 	new_node->ac_id = ac_id;
 	node->next = new_node;
-	return new_node;	
+	return new_node;
+
 }
 
 
