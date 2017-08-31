2fd103ffe8962051b62a089570c4741a420a1153
finkeneers <finkeneers@iws.cs.ovgu.de>
2017-08-31 14:08:19 +0200

    used autopilot_set_mode() to change to NAV-Mode

--- sw/airborne/modules/copter_swarm/copter_swarm.c
+++ sw/airborne/modules/copter_swarm/copter_swarm.c
@@ -8,8 +8,10 @@
 #include <math.h>
 #include <subsystems/ins/ins_int.h>
 #include <firmwares/rotorcraft/navigation.h>
+#include <firmwares/rotorcraft/guidance.h>
+#include <firmwares/rotorcraft/autopilot.h>
 
-
+const float insToMeter = 0.0039063;
 
 static ins_node_t swarm[SWARM_SIZE];
 
@@ -17,14 +19,11 @@ void copter_swarm_init( void ) {
 	int i = 0;
 	for( ; i < SWARM_SIZE; i++){
 		swarm[i].ac_id=-1;
-	}
+	}	
 }
-
 
 void copter_ins_action(void) {
 
-
-	
 	//To get the current copter id
 	int copter_ac_id = DL_COPTER_INS_ac_id(dl_buffer);
 	// we already know own position so skip this.
@@ -103,9 +102,14 @@ void copter_swarm_periodic(void)
 	// TODO transform forces to speed command
 	struct EnuCoor_i myPos = *(stateGetPositionEnu_i());
 	
+	//guidance_h_mode_changed(GUIDANCE_H_MODE_NAV);
+	autopilot_set_mode(AP_MODE_NAV);
 
-	navigation_carrot.x = myPos.x + fx_sum;
-	navigation_carrot.y = myPos.y + fy_sum;
+	//navigation_carrot.x = myPos.x + fx_sum;
+	//navigation_carrot.y = myPos.y + fy_sum;
+
+	guidance_h_pos_sp.x = myPos.x + fx_sum;
+	guidance_h_pos_sp.y = myPos.y + fy_sum;
 
 	// speed command?
 	//guidance_h_set_guided_vel(float vx, float vy)
@@ -122,14 +126,16 @@ void calcForce(ins_node_t* copter0, double* fx_out, double* fy_out){
 	copter1.ins_y = ins_int.ltp_pos.y;
 	copter1.ins_z = ins_int.ltp_pos.z;
 
-	float cons = 0.1;
-	float d = 1.0;
+	float cons = 10.0;
+	float d = 0.5;
 	float diff_x;
 	float diff_y;
 	double dist;
 
 	diff_x = copter1.ins_x - copter0->ins_x;
 	diff_y = copter1.ins_y - copter0->ins_y; 
+	diff_x = diff_x * insToMeter;
+	diff_y = diff_y * insToMeter;
 	dist = sqrt((diff_x*diff_x) + (diff_y*diff_y));	
 
 	if(dist < d){
