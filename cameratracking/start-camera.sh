055c73aeea29682ed0f9aa08415b48cc08b741c5
Simon Parlow <simonparlow@gmx.net>
2018-04-22 18:37:43 +0200

    Pre add subtree

---
+++ cameratracking/start-camera.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Source setup.bash
+cd Finken-SwarmBehavior/devel
+source setup.bash
+cd ../..
+
+# Start Ros-Core
+
+
---
+++ scripts/start-paparazzi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#cd paparazzi &&
+    #make clean;
+    sudo conf/system/ftdi.sh &&
+#    make &&
+    ./paparazzi
