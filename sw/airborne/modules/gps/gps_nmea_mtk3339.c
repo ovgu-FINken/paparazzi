56dc481a91de86945c1cf3aa536a0525c6179df3
Christoph Steup <steup@ivs.cs.ovgu.de>
2018-09-10 17:48:27 +0200

    MTK3339: fix various busg + add VTG sentence support

--- sw/airborne/modules/gps/gps_nmea_mtk3339.c
+++ sw/airborne/modules/gps/gps_nmea_mtk3339.c
@@ -4,15 +4,15 @@
   #error "Undefined baudrate for mtk3339"
 #endif
 
-#ifndef MTK_RATE
+#ifndef MTK_PERIOD
   #error "Undefined update rate for mtk3339"
 #endif
 
 #define MTK_BAUD_STRING STRINGIFY(MTK_BAUD)
-#define MTK_UPDATE_STRING STRINGIFY(1000/MTK_UPDATE)
+#define MTK_PERIOD_STRING STRINGIFY(MTK_PERIOD)
 
 #define MTK_BAUD_RATE_CMD "$PMTK251,"
-#define MTK_UPDATE_RATE_CMD "$PMTK220,"
+#define MTK_UPDATE_PERIOD_CMD "$PMTK220,"
 
 static uint8_t toHex(uint8_t digit){
   return digit<10?digit+'0':digit-10+'A';
@@ -44,20 +44,89 @@ void nmea_configure(void) {
   sendCmd(MTK_BAUD_RATE_CMD, sizeof(MTK_BAUD_RATE_CMD)-1, 
           MTK_BAUD_STRING  , sizeof(MTK_BAUD_STRING)-1  );
   //Wait for the transmission to finish
-  while(!dev->check_free_space(dev->periph, NULL, UART_TX_BUFFER_SIZE-1));
+  for(uint8_t i=0;i<100;i++)
+    if(dev->check_free_space(dev->periph, NULL, UART_TX_BUFFER_SIZE-1))
+      break;
   //Change the baud rate of the UART
   dev->set_baudrate(dev->periph, MTK_BAUD);
-  sendCmd(MTK_UPDATE_RATE_CMD, sizeof(MTK_UPDATE_RATE_CMD)-1, 
-          MTK_UPDATE_STRING  , sizeof(MTK_UPDATE_STRING)-1  );
   //Done
   gps_nmea.is_configured = true;
 }
 
-void nmea_parse_prop_init(void) {
+static uint8_t findNextComma(uint8_t pos) {
+  for(uint8_t i=pos;i<gps_nmea.msg_len;i++)
+    if(gps_nmea.msg_buf[i]==',')
+      return i;
+  return gps_nmea.msg_len;
+}
 
+static bool findNextDatum(uint8_t* pos, uint8_t* next, char c) {
+
+  uint8_t start = findNextComma(*pos)+1;
+  uint8_t end = findNextComma(start);
+  uint8_t end2 = findNextComma(end+1);
+  if(end2-end == 2 && gps_nmea.msg_buf[end+1]==c) {
+    gps_nmea.msg_buf[end]='\0';
+    *pos=start;
+    *next=end2;
+    return true;
+  } else
+    return false;
+}
+
+static bool nmea_parse_vtg(void) {
+  uint8_t pos=0;
+  uint8_t next;
+  if(!findNextDatum(&pos, &next, 'T'))
+    return false;
+  gps_nmea.state.course = atof(gps_nmea.msg_buf+pos);
+  pos=next;
+  if(!findNextDatum(&pos, &next, 'M'))
+    return false;
+  pos=next;
+  if(!findNextDatum(&pos, &next, 'N'))
+    return false;
+  pos=next;
+  if(!findNextDatum(&pos, &next, 'K'))
+    return false;
+  gps_nmea.state.gspeed = atof(gps_nmea.msg_buf+pos)*1000/36;
+  return true;
+}
+
+enum mtk_states {
+  INIT,
+  DONE
+};
+
+static enum mtk_states mtk_state;
+
+static bool nmea_parse_ack(void) {
+  uint8_t pos=findNextComma(0)+1;
+  uint8_t next=findNextComma(pos);
+  gps_nmea.msg_buf[next]='\0';
+  uint8_t cmd = atoi(gps_nmea.msg_buf+pos);
+  pos=next+1;
+  next=findNextComma(pos);
+  gps_nmea.msg_buf[next]='\0';
+  uint8_t state = atoi(gps_nmea.msg_buf+pos);
+  if(cmd==220 && state==3)
+    mtk_state=DONE;
+  return true;
+}
+
+
+void nmea_parse_prop_init(void) {
+  mtk_state=INIT;
 }
 
 bool nmea_parse_prop_msg(void) {
-  //Parse VTG Messages
+  gps_nmea.state.pacc=mtk_state;
+  if(mtk_state==INIT) 
+    sendCmd(MTK_UPDATE_PERIOD_CMD, sizeof(MTK_UPDATE_PERIOD_CMD)-1, 
+            MTK_PERIOD_STRING  , sizeof(MTK_PERIOD_STRING)-1  );
+  if( gps_nmea.msg_len > 5 && !strncmp(&gps_nmea.msg_buf[2], "VTG", 3))
+    return nmea_parse_vtg();
+  if( gps_nmea.msg_len > 5 && !strncmp(&gps_nmea.msg_buf[0], "PMTK001", 7))
+    return nmea_parse_ack();
   return false;
 }
